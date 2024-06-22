a570dfd6686ce18a883262a7686e873ebb877074
PhucLoc <phuclocub@gmail.com>
2024-06-22 15:06:06 +0700

    api ctsp add chatlieu field

--- src/main/java/com/example/java4/RestControllers/ChiTietSPController.java
+++ src/main/java/com/example/java4/RestControllers/ChiTietSPController.java
@@ -104,7 +104,7 @@ public class ChiTietSPController {
           } else{
               chiTietSP.setIdMauSac(newChiTietSP.getIdMauSac());
               chiTietSP.setIdKichThuoc(newChiTietSP.getIdKichThuoc());
-              chiTietSP.setIdMauSac(newChiTietSP.getIdMauSac());
+              chiTietSP.setIdChatLieu(newChiTietSP.getIdChatLieu());
               chiTietSP.setNamBH(Integer.valueOf(newChiTietSP.getNamBH()));
               chiTietSP.setMoTa(newChiTietSP.getMoTa());
               chiTietSP.setSoLuongTon(Integer.valueOf(newChiTietSP.getSoLuongTon()));
@@ -149,7 +149,7 @@ public class ChiTietSPController {
             chiTietSP.setIdSp(newChiTietSP.getIdSp());
             chiTietSP.setIdMauSac(newChiTietSP.getIdMauSac());
             chiTietSP.setIdKichThuoc(newChiTietSP.getIdKichThuoc());
-            chiTietSP.setIdMauSac(newChiTietSP.getIdMauSac());
+            chiTietSP.setIdChatLieu(newChiTietSP.getIdChatLieu());
             chiTietSP.setNamBH(Integer.valueOf(newChiTietSP.getNamBH()));
             chiTietSP.setMoTa(newChiTietSP.getMoTa());
             chiTietSP.setSoLuongTon(Integer.valueOf(newChiTietSP.getSoLuongTon()));
